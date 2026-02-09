cba5bb6280b24718cd63092624f7be099a51d9f3
Rebeckawestborg <rwestborg@gmail.com>
2026-02-09 17:16:50 +0100

    videre arbeid med components
    lagt inn shoppingitems, prøver å finne ut .map() på shoppinglist.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,15 +2,27 @@ import { useState } from 'react'
 import './App.css'
 import AddForm from './components/AddForm'
 import ShoppingList from './components/ShoppingList'
-import ShoppingItem from './components/ShoppingItem'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+        const shoppingItems = [
+        {
+            id: 0,
+            title: "Melk"
+        },
+        {
+            id: 1,
+            title: "Egg"
+        }
+    ]
+  const [shoppingList, setshoppingList] = useState(shoppingItems)
+
 
   return (
     <main>
+      <h1>Handleliste</h1>
       <AddForm />
-      <ShoppingList/>
+      <ShoppingList ShoppingItem={ShoppingItem}/>
       <ShoppingItem/>
 
     </main>
--- my-react-app/src/components/ShoppingList.jsx
+++ my-react-app/src/components/ShoppingList.jsx
@@ -4,14 +4,13 @@ import ShoppingItem from "./ShoppingItem";
   Todos-komponenten har ansvar for å håndtere
   og vise en samling med todo-oppgaver.
 */
-export default function ShoppingList({shoppingItems, setShoppingList}){
+export default function ShoppingList({shoppingItems})
 
     /*
       Dette er en liste med todo-objekter.
       Hvert objekt representerer én oppgave og inneholder:
       - id: unik identifikator (viktig når man renderer lister i React)
       - title: kort tittel på oppgaven
-      - content: mer detaljert beskrivelse
       
       Vi bruker en array for å enkelt kunne jobbe med flere todos.
     */
@@ -24,9 +23,9 @@ export default function ShoppingList({shoppingItems, setShoppingList}){
               å vise én enkelt todo.
                Senere kan vi bruke todoItems-arrayet sammen
               med map() for å sende data til Todo-komponenten
-              via props og vise alle oppgavene dynamisk.
-            *
-            {shoppingItem.map((item) => <Todo key={item.id} title={item.title} content={item.content} id={item.id} setShoppingList={setShoppingList} />)}*/}
+              via props og vise alle oppgavene dynamisk.*/
+            
+            {shoppingItems.map((item) => <ShoppingItem key={item.id} title={item.title} id={item.id} setShoppingList={setShoppingList} />)}}
         </section>
     )
 }
